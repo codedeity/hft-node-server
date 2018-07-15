692310d2fe1fbc08d833b0247ca6e717d40ae254
Edward <chenlulei@live.com>
2018-07-15 21:36:53 +0800

    update README and config

--- config/config.default.js
+++ config/config.default.js
@@ -4,7 +4,7 @@ module.exports = appInfo => {
   const config = exports = {};
 
   // use for cookie sign key, should change to your own and keep security
-  config.keys = appInfo.name + '_1531660039610_7822';
+  config.keys = appInfo.name + '_hft_team_1531660039610_7822';
 
   // add your config here
   config.middleware = [];
